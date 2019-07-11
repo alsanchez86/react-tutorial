35cf958cc7fe2aa8b6eeeeea6aa01f6e1d95729b
alsanchez86 <sanchezpizarro86@gmail.com>
2019-07-11 17:40:21 +0200

    install react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 /**
  * TODO:
- * - Probar componentes de https://github.com/reactstrap/reactstrap.
- * - Probar un router de reactjs para el manejo de rutas en la SPA.
+ * - Probar componentes de https://github.com/reactstrap/reactstrap. -> OK
+ * - Probar un router de reactjs para el manejo de rutas en la SPA. -> https://es.redux.js.org/docs/avanzado/uso-con-react-router.html
  * - Hacer una request a una API pública para probar los services de react: https://github.com/axios/axios
  * - Usar Rambda o Lodash para la programación funcional.
  * - Probar Typescript para ver que nos aporta.
- * - Meter Redux(flux) para el control de estado de la APP.
+ * - Meter Redux(flux) para el control de estado de la APP. -> https://www.youtube.com/watch?v=OXWn4XiDUmw
  * - Usar inmutable.js para mantener la inmutabilidad del estado.
  * - https://es.redux.js.org/docs/introduccion/herencia.html#rx
 */
