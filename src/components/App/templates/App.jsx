fa350e1481dc98bf9ecec8d01bf08d5dcfca6374
alsanchez86 <sanchezpizarro86@gmail.com>
2019-07-29 15:30:32 +0200

    update styles

--- src/components/App/templates/App.jsx
+++ src/components/App/templates/App.jsx
@@ -14,17 +14,6 @@ export default (p) =>
 
     <Header/>
 
-    {/* <header className="masthead mb-auto">
-        <div className="inner">
-            <h3 className="masthead-brand">Cover</h3>
-            <nav className="nav nav-masthead justify-content-center">
-                <a className="nav-link active" href="#">Home</a>
-                <a className="nav-link" href="#">Features</a>
-                <a className="nav-link" href="#">Contact</a>
-            </nav>
-        </div>
-    </header> */}
-
     <main role="main" className="inner cover">
         <Main/>
         {/* <h1 className="cover-heading">Cover your page.</h1>
--- src/components/Header/templates/Header.jsx
+++ src/components/Header/templates/Header.jsx
@@ -12,8 +12,19 @@ import {
 
 export default (p) =>
 
-<header>
-    <Navbar color="light" light expand="md">
+<header className="masthead mb-auto">
+
+        <div className="inner">
+            <h3 className="masthead-brand">Cover</h3>
+            <nav className="nav nav-masthead justify-content-center">
+                <a className="nav-link active" href="#">Home</a>
+                <a className="nav-link" href="#">Features</a>
+                <a className="nav-link" href="#">Contact</a>
+            </nav>
+        </div>
+
+
+    {/* <Navbar color="light" light expand="md">
         <Nav navbar>
             <NavItem>
                 <NavLink tag={() =>
@@ -31,5 +42,5 @@ export default (p) =>
                 }/>
             </NavItem>
         </Nav>
-    </Navbar>
+    </Navbar> */}
 </header>
