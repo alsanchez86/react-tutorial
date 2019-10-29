52fad68a1a19f9bb171a7d617bc78ee6223ae7bd
alsanchez86 <sanchezpizarro86@gmail.com>
2019-10-29 16:03:45 +0100

    fix mapStateToProps square

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -81,14 +81,16 @@ class Square extends Component<any> {
 
 export default connect(
     // mapStateToProps
-    (state: any): object => (() => {
-        let winner = get(state, "boardReducer.winner", boardStateWinner);
-        return {
-            xIsNext: get(state, "boardReducer.xIsNext", boardStateXIsNext),
-            winner: winner,
-            disabled: get(state, "boardReducer.draw", boardStateDraw) || (winner.length > 0) || (get(state, "boardReducer.step", boardStateStep) < (get(state, "boardReducer.history", boardStateHistory).length - 1))
-        };
-    }),
+    (state: any): object => ((
+        function() {
+            let winner = get(state, "boardReducer.winner", boardStateWinner);
+            return {
+                xIsNext: get(state, "boardReducer.xIsNext", boardStateXIsNext),
+                winner: winner,
+                disabled: (get(state, "boardReducer.draw", boardStateDraw) || (winner.length > 0) || (get(state, "boardReducer.step", boardStateStep) < (get(state, "boardReducer.history", boardStateHistory).length - 1)))
+            }
+        }
+    )()),
     // mapDispatchToProps
     (dispatch: Function): object => ({
         onClick: (y: number, x: number, mark: string) => dispatch(markSquare(y, x, mark))
--- src/redux/board/default.tsx
+++ src/redux/board/default.tsx
@@ -1,11 +1,7 @@
 // Interface: BoardState
 export const boardStateCells: string[][] = Array(3).fill(Array(3).fill(""));
 export const boardStateWinner: number[] = [];
-export const boardStateHistory: string[][][] = [
-    [
-        []
-    ]
-];
+export const boardStateHistory: string[][][] = [];
 export const boardStateDraw: boolean = false;
 export const boardStateStep: number = 0;
 export const boardStateXIsNext: boolean = false;
